103e6f22c5b25c9032837147f34c30acd5ab48b4
Dalmus <dalmus@gmail.com>
2026-02-18 17:09:36 +0100

    Add simple shell comnads to deploy Argocd using helm

---
+++ deploy_argocd_using_helm.sh
@@ -0,0 +1,9 @@
+helm repo add argocd https://argoproj.github.io/argo-helm
+helm repo update
+helm install argocd argocd/argo-cd
+kubectl port-forward service/argocd-server -n default 8181:443
+
+https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/#option-1-ssl-passthrough
+https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/#option-2multiple-ingress-objects-and-hosts
+kubectl -n default get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d
+
