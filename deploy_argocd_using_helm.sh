0884b0b40d13be125fc97aefc411e1f22802270d
Dalmus <dalmus@gmail.com>
2026-02-23 11:41:28 +0100

    change namespace for mimir.sync

--- deploy_argocd_using_helm.sh
+++ deploy_argocd_using_helm.sh
@@ -5,5 +5,5 @@ kubectl port-forward service/argocd-server -n default 8181:443
 
 https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/#option-1-ssl-passthrough
 https://argo-cd.readthedocs.io/en/stable/operator-manual/ingress/#option-2multiple-ingress-objects-and-hosts
-kubectl -n default get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d
 
