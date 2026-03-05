72edd8fae7535d97ff42ceb49354f45c434ff662
Dalmus <dalmus@gmail.com>
2026-03-05 16:17:32 +0100

    fix: Postgres versipn set to 16

--- pgsql/postgres_deploy.sh
+++ pgsql/postgres_deploy.sh
@@ -2,6 +2,7 @@ kubectl create namespace db --dry-run=client -o yaml | kubectl apply -f -
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm install pg bitnami/postgresql -n db \
+  --set imgiage.tag=16 \
   --set auth.postgresPassword='postgresmin' \
   --set auth.username='guacamole' \
   --set auth.password='Gu@c@m0l3' \
